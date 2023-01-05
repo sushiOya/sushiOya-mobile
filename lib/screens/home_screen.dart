aeb4ba5a0ed54d14088859d2c36c2bd0407d817f
falsystack <falsystack@gmail.com>
2023-01-05 13:04:17 +0900

    엔트리, 미들, 하이엔드 카테고리 디자인구현

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final FocusNode inputFocus = FocusNode();
 
   @override
   Widget build(BuildContext context) {
@@ -40,13 +47,20 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Container(
-            color: Colors.white,
-            padding: const EdgeInsets.fromLTRB(16, 12, 16, 24),
-            child: TextField(
-              decoration: InputDecoration(
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            inputFocus.unfocus();
+          });
+        },
+        child: Column(
+          children: [
+            Container(
+              color: Colors.white,
+              padding: const EdgeInsets.fromLTRB(16, 12, 16, 24),
+              child: TextField(
+                focusNode: inputFocus,
+                decoration: InputDecoration(
                   enabledBorder: const UnderlineInputBorder(
                       borderSide: BorderSide(color: Colors.black)),
                   hintStyle: const TextStyle(
@@ -55,70 +69,209 @@ class HomeScreen extends StatelessWidget {
                     fontWeight: FontWeight.w400,
                   ),
                   hintText: '찾고있는 오마카세가 있으신가요?',
-                  suffixIcon: Image.asset('asset/images/icons/search.png')),
-            ),
-          ),
-          Stack(
-            children: [
-              Container(
-                width: double.infinity,
-                height: 159,
-                child: Image.asset('asset/images/banner/main_banner.png',
-                    fit: BoxFit.fitWidth),
+                  suffixIcon: Image.asset('asset/images/icons/search.png'),
+                ),
               ),
-              Positioned(
-                bottom: 27,
-                left: 16,
-                child: Container(
-                  width: 113,
-                  height: 33,
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 7, horizontal: 24),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all(width: 1.0, color: Colors.white),
-                  ),
-                  child: OutlinedButton(
-                    onPressed: () {
-                      print('Clicked!');
-                    },
-                    style: OutlinedButton.styleFrom(
-                        padding: const EdgeInsets.all(0),
-                        side: const BorderSide(
-                          width: 0,
-                          color: Colors.transparent,
-                        )),
-                    child: const Text(
-                      '설문하러가기',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 12,
-                        fontWeight: FontWeight.w400,
+            ),
+            Stack(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 159,
+                  child: Image.asset('asset/images/banner/main_banner.png',
+                      fit: BoxFit.fitWidth),
+                ),
+                Positioned(
+                  bottom: 27,
+                  left: 16,
+                  child: Container(
+                    width: 113,
+                    height: 33,
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 7, horizontal: 24),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      border: Border.all(width: 1.0, color: Colors.white),
+                    ),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        print('Clicked!');
+                      },
+                      style: OutlinedButton.styleFrom(
+                          padding: const EdgeInsets.all(0),
+                          side: const BorderSide(
+                            width: 0,
+                            color: Colors.transparent,
+                          )),
+                      child: const Text(
+                        '설문하러가기',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              Positioned(
-                top: 24,
-                left: 16,
-                child: Container(
-                  width: 201,
-                  height: 56,
-                  padding: const EdgeInsets.all(0),
-                  child: const Text(
-                    '설문 참여하고 \n오마카세 런치 받아가세요',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w700,
+                Positioned(
+                  top: 24,
+                  left: 16,
+                  child: Container(
+                    width: 201,
+                    height: 56,
+                    padding: const EdgeInsets.all(0),
+                    child: const Text(
+                      '설문 참여하고 \n오마카세 런치 받아가세요',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
                     ),
                   ),
                 ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 37, 16, 32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      print('entry');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shadowColor: Colors.black.withOpacity(0.8),
+                      backgroundColor: Colors.white,
+                      padding: EdgeInsets.all(0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
+                    ),
+                    child: Container(
+                      width: 100,
+                      height: 88,
+                      padding: const EdgeInsets.all(8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            '엔트리',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                            ),
+                            textAlign: TextAlign.start,
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Text(
+                            '런치\n오만원 까지',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                              height: 1.4,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print('entry');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shadowColor: Colors.black.withOpacity(0.8),
+                      backgroundColor: Colors.white,
+                      padding: EdgeInsets.all(0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
+                    ),
+                    child: Container(
+                      width: 100,
+                      height: 88,
+                      padding: const EdgeInsets.all(8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            '미들',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                            ),
+                            textAlign: TextAlign.start,
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Text(
+                            '런치\n십만원 까지',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                              height: 1.4,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print('entry');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shadowColor: Colors.black.withOpacity(0.8),
+                      backgroundColor: Colors.white,
+                      padding: EdgeInsets.all(0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
+                    ),
+                    child: Container(
+                      width: 100,
+                      height: 88,
+                      padding: const EdgeInsets.all(8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            '하이엔드',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                            ),
+                            textAlign: TextAlign.start,
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Text(
+                            '런치\n십만원 이상',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                              height: 1.4,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
